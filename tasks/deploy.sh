7212bec658e1dc0d0efad65b8867d7023d7c6916
Fred McHale <binaryfever@gmail.com>
2013-02-09 21:39:24 -0800

    updated compile and deploy script to actually compile

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -1,1 +1,1 @@
-rsync -e ssh -av ~/Development/blog/_site/* fmchale@binaryfever.com:/home/fmchale/www/binaryfever.com
+jekyll && rsync -e ssh -av ~/Development/blog/_site/* fmchale@binaryfever.com:/home/fmchale/www/binaryfever.com
